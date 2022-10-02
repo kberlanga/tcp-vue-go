56aee4a9393e2ce2aebd31f542cc5bd3da297537
kberlanga <karla_berlanga28@hotmail.com>
2022-10-02 17:04:09 -0500

    feat: :sparkles: add filters in connections & stadistics page

--- app/src/components/ConnectionsPage.vue
+++ app/src/components/ConnectionsPage.vue
@@ -4,11 +4,40 @@
       :headers="headers"
       :items="connections"
       :items-per-page="10"
-      :height="550"
+      :height="500"
       :fixed-header="true"
       :loading="false"
       class="elevation-1"
     ></v-data-table>
+    <div class="content-table__filters">
+      <h3>Filters</h3>
+      <section class="content-table__filters__selects">
+        <v-select
+          v-model="filters.client"
+          :items="clients"
+          item-text="label"
+          label="Client"
+          @change="filter()"
+          return-object
+          outlined
+        ></v-select>
+        <v-select
+          v-model="filters.channel"
+          :items="channels"
+          item-text="label"
+          label="Channel"
+          outlined
+          return-object
+          @change="filter()"
+        ></v-select>
+        <section class="content-table__filters__buttons">
+          <v-btn v-on:click="cleanFilters">
+            <v-icon left> mdi-eraser </v-icon>
+            Clear</v-btn
+          >
+        </section>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -24,21 +53,98 @@ export default {
         { text: "Channel", value: "channel" },
         { text: "Subscription Time", value: "subscriptionTime" },
       ],
+      connections: [],
+      filters: {
+        client: { value: "", label: "None" },
+        channel: { value: "", label: "None" },
+      },
     };
   },
+  created() {
+    this.connections = this.conns.map((connection) => {
+      return {
+        ...connection,
+        subscriptionTime: new Date(
+          connection.subscriptionTime
+        ).toLocaleString(),
+      };
+    });
+  },
   computed: {
-    connections: {
+    data() {
+      return this.conns.map((connection) => {
+        return {
+          ...connection,
+          subscriptionTime: new Date(
+            connection.subscriptionTime
+          ).toLocaleString(),
+        };
+      });
+    },
+    clients: {
       get: function () {
-        return this.conns.map((connection) => {
-          return {
-            ...connection,
-            subscriptionTime: new Date(
-              connection.subscriptionTime
-            ).toLocaleString(),
-          };
-        });
+        return [
+          { value: "", label: "None" },
+          ...this.conns.map((connection) => {
+            return { value: connection.client, label: connection.client };
+          }),
+        ];
       },
+    },
+    channels: {
+      get: function () {
+        return [
+          { value: "", label: "None" },
+          ...this.conns.map((connection) => {
+            return { value: connection.channel, label: connection.channel };
+          }),
+        ];
+      },
+    },
+  },
+  watch: {
+    data: {
+      deep: true,
+      handler: function (newVal) {
+        this.connections = newVal;
+      },
+    },
+  },
+  methods: {
+    filterByClient: function (connections) {
+      return connections.filter((connection) => {
+        return connection.client === this.filters.client.value;
+      });
+    },
+    filterByChannel: function (connections) {
+      return connections.filter((connection) => {
+        return connection.channel === this.filters.channel.value;
+      });
+    },
+    filter: function () {
+      let filtered = this.data;
+
+      if (this.filters.channel.value !== "") {
+        filtered = this.filterByChannel(filtered);
+      }
+
+      if (this.filters.client.value !== "") {
+        filtered = this.filterByClient(filtered);
+      }
+
+      this.connections = filtered;
+    },
+    cleanFilters: function () {
+      this.connections = this.data;
+      this.filters = {
+        client: { value: "", text: "None" },
+        channel: { value: "", text: "None" },
+      };
     },
   },
 };
 </script>
+
+<style>
+@import "../assets/styles/table-filters.css";
+</style>
--- app/src/components/StadisticsPage.vue
+++ app/src/components/StadisticsPage.vue
@@ -4,7 +4,7 @@
       :headers="headers"
       :items="stadistics"
       :items-per-page="10"
-      :height="630"
+      :height="500"
       :fixed-header="true"
       :loading="false"
       group-by="toChannel"
@@ -17,6 +17,44 @@
         </v-chip>
       </template>
     </v-data-table>
+    <div class="content-table__filters">
+      <h3>Filters</h3>
+      <section class="content-table__filters__selects">
+        <v-select
+          v-model="filters.status"
+          :items="status"
+          item-text="label"
+          label="Status"
+          @change="filter()"
+          return-object
+          outlined
+        ></v-select>
+        <v-select
+          v-model="filters.client"
+          :items="clients"
+          item-text="label"
+          label="Client"
+          @change="filter()"
+          return-object
+          outlined
+        ></v-select>
+        <v-select
+          v-model="filters.channel"
+          :items="channels"
+          item-text="label"
+          label="Channel"
+          @change="filter()"
+          return-object
+          outlined
+        ></v-select>
+        <section class="content-table__filters__buttons">
+          <v-btn v-on:click="cleanFilters">
+            <v-icon left> mdi-eraser </v-icon>
+            Clear</v-btn
+          >
+        </section>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -36,21 +74,69 @@ export default {
       ],
       statusColors: {
         PENDING: "#a1a1a1",
-        RECEIVING: "#64b2d9",
         SUCCESS: "#9cc295",
+      },
+      stadistics: [],
+      filters: {
+        client: { value: "", label: "None" },
+        channel: { value: "", label: "None" },
+        status: { value: "", label: "None" },
       },
     };
   },
+  created() {
+    this.stadistics = this.stds.map((stadistic) => {
+      return {
+        ...stadistic,
+        shippingTime: this.formatDate(stadistic.shippingTime),
+        receiptTime: this.formatDate(stadistic.receiptTime),
+      };
+    });
+  },
   computed: {
-    stadistics: {
+    data() {
+      return this.stds.map((stadistic) => {
+        return {
+          ...stadistic,
+          shippingTime: this.formatDate(stadistic.shippingTime),
+          receiptTime: this.formatDate(stadistic.receiptTime),
+        };
+      });
+    },
+    clients: {
       get: function () {
-        return this.stds.map((stadistic) => {
-          return {
-            ...stadistic,
-            shippingTime: this.formatDate(stadistic.shippingTime),
-            receiptTime: this.formatDate(stadistic.receiptTime),
-          };
-        });
+        return [
+          { value: "", label: "None" },
+          ...this.stds.map((stadistic) => {
+            return { value: stadistic.toClient, label: stadistic.toClient };
+          }),
+        ];
+      },
+    },
+    channels: {
+      get: function () {
+        return [
+          { value: "", label: "None" },
+          ...this.stds.map((stadistic) => {
+            return { value: stadistic.toChannel, label: stadistic.toChannel };
+          }),
+        ];
+      },
+    },
+    status() {
+      return [
+        { value: "", label: "None" },
+        ...Object.keys(this.statusColors).map((status) => {
+          return { value: status, label: status };
+        }),
+      ];
+    },
+  },
+  watch: {
+    stadistics: {
+      deep: true,
+      handler: function (newVal) {
+        this.stadistics = newVal;
       },
     },
   },
@@ -65,6 +151,45 @@ export default {
     },
     getColor: function (status) {
       return this.statusColors[status] ? this.statusColors[status] : "white";
+    },
+    filterByStatus: function (stadistics) {
+      return stadistics.filter((stadistic) => {
+        return stadistic.status === this.filters.status.value;
+      });
+    },
+    filterByClient: function (stadistics) {
+      return stadistics.filter((stadistic) => {
+        return stadistic.toClient === this.filters.client.value;
+      });
+    },
+    filterByChannel: function (stadistics) {
+      return stadistics.filter((stadistic) => {
+        return stadistic.toChannel === this.filters.channel.value;
+      });
+    },
+    filter: function () {
+      let filtered = this.data;
+      if (this.filters.status.value !== "") {
+        filtered = this.filterByStatus(filtered);
+      }
+
+      if (this.filters.channel.value !== "") {
+        filtered = this.filterByChannel(filtered);
+      }
+
+      if (this.filters.client.value !== "") {
+        filtered = this.filterByClient(filtered);
+      }
+
+      this.stadistics = filtered;
+    },
+    cleanFilters: function () {
+      this.stadistics = this.data;
+      this.filters = {
+        client: { value: "", label: "None" },
+        channel: { value: "", label: "None" },
+        status: { value: "", label: "None" },
+      };
     },
   },
 };
